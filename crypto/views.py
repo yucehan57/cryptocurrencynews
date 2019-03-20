675380299425501a7bde84c008efadf2ea4ed709
yucehan.kucukmotor <ychnkckmtr@gmail.com>
2019-03-19 23:57:03 -0400

    fetch api data for news and price data. Add bootstrap cards and table

--- crypto/views.py
+++ crypto/views.py
@@ -6,9 +6,21 @@ def home(request):
     """Function-based home view"""
     import requests
     import json
+
+
+    # grab currency trade price
+    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP&tsyms=USD,EUR,TRY")
+    price = json.loads(price_request.content)
+
+
+
+    # grab crpyto currency news
     api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api = json.loads(api_request.content)
+
+    # pass the data
     context = {
-        'api': api
+        'api': api,
+        'price': price,
     }
     return render(request, 'home.html', context)
