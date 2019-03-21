865e9cada6a2cd33932492714f36355dd71fdf06
yucehan.kucukmotor <ychnkckmtr@gmail.com>
2019-03-20 23:49:27 -0400

    search button functionality added, prices template modified for logic

--- crypto/views.py
+++ crypto/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+import requests
+import json
+
 
 def home(request):
     """Function-based home view"""
-    import requests
-    import json
-
 
     # grab currency trade price
     price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP&tsyms=USD,EUR,TRY")
@@ -24,3 +23,23 @@ def home(request):
         'price': price,
     }
     return render(request, 'home.html', context)
+
+
+def prices(request):
+    """Get the price quote using the search button"""
+    if request.method == 'POST':
+        """pass in 'quote' from search bar name in price.html"""
+        quote = request.POST['quote']
+        quote = quote.upper()
+        crypto_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + quote + "&tsyms=USD,EUR,TRY")
+        crypto = json.loads(crypto_request.content)
+        context = {
+            'quote': quote,
+            'crypto': crypto,
+        }
+        return render(request, 'prices.html', context)
+    else:
+        notfound = 'Ticker for Crypto currency is not recognized. Please choose a valid three-letter ticker such as XRP for Ripple'
+        return render(request, 'prices.html', {
+                                        'notfound': notfound,
+                                              })
